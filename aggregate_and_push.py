22577f48bbb3859dca3f62872cfa22806697e4ee
George Nagel <gsnagel@gmail.com>
2025-07-24 08:41:51 -0400

    Update cron

--- aggregate_and_push.py
+++ aggregate_and_push.py
@@ -3,7 +3,6 @@ import os
 from datetime import datetime
 
 subprocess.run(["python3", "aggregate_statistics.py"], check=True)
-
 subprocess.run(["git", "add", "."], check=True)
 subprocess.run(
     ["git", "commit", "-m", f"Auto update {datetime.now().isoformat()}"], check=True

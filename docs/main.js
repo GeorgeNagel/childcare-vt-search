7e259ff144e46603cf5000cbdc133f6e15243c16
George Nagel <gsnagel@gmail.com>
2025-08-20 15:22:51 -0400

    Update chart js

--- docs/main.js
+++ docs/main.js
@@ -1,30 +1,46 @@
 fetch("statistics.json")
   .then(response => response.json())
   .then(data => {
-    const availability = data.availability;
+    const statsByDate = data["stats_by_date"];
 
-    const dates = Object.keys(availability).sort();
-    const infantData = dates.map(date => availability[date].infant);
-    const toddlerData = dates.map(date => availability[date].toddler);
+    const dates = Object.keys(statsByDate).sort();
+    const infantAvailabilityData = dates.map(date => statsByDate[date]["infant_availability"]);
+    const toddlerAvailabilityData = dates.map(date => statsByDate[date]["toddler_availability"]);
+    const infantCapacityData = dates.map(date => statsByDate[date]["infant_capacity"]);
+    const toddlerCapacityData = dates.map(date => statsByDate[date]["toddler_capacity"]);
 
-    const ctx = document.getElementById('availabilityChart').getContext('2d');
+    const ctx = document.getElementById('childcareStatsByDateChart').getContext('2d');
     new Chart(ctx, {
       type: 'line',
       data: {
         labels: dates,
         datasets: [
           {
-            label: 'Infant',
-            data: infantData,
+            label: 'Infant Availability',
+            data: infantAvailabilityData,
             borderColor: 'rgba(255, 99, 132, 1)',
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             fill: false,
           },
           {
-            label: 'Toddler',
-            data: toddlerData,
+            label: 'Toddler Availability',
+            data: toddlerAvailabilityData,
             borderColor: 'rgba(54, 162, 235, 1)',
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            fill: false,
+          }
+          {
+            label: 'Infant Availability',
+            data: infantCapacityData,
+            borderColor: 'rgb(255, 119, 52)',
+            backgroundColor: 'rgba(206, 140, 34, 0.2)',
+            fill: false,
+          },
+          {
+            label: 'Toddler Availability',
+            data: toddlerCapacityData,
+            borderColor: 'rgb(202, 93, 245)',
+            backgroundColor: 'rgba(180, 21, 191, 0.2)',
             fill: false,
           }
         ]
@@ -33,7 +49,7 @@ fetch("statistics.json")
         responsive: true,
         scales: {
           x: { title: { display: true, text: 'Date' } },
-          y: { title: { display: true, text: 'Vacancies' }, beginAtZero: true }
+          y: { title: { display: true, text: 'Total Spots' }, beginAtZero: true }
         }
       }
     });
